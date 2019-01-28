7321b615bd26b58f390dadbba909ea56e21bece9
Morrow Davidson <morrow.davidson@gmail.com>
2019-01-28 11:52:34 -0800

    Add tie condition

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-
+let playCounter = 0;
 let currentPlayer = 'O';
 
 let e = "empty";
@@ -18,7 +18,15 @@ function playTrigger(squareId){
 
         square.innerHTML = currentPlayer; 
 
-        checkWinCondition();
+        playCounter += 1;
+
+        if (checkWinCondition()){
+            return;
+        };
+
+        if (checkTieCondition()){
+            return;
+        };
 
         if (currentPlayer == 'X'){
             currentPlayer = 'O';
@@ -32,28 +40,48 @@ function playTrigger(squareId){
 function checkWinCondition(){
     if ((gameBoard['tl'] != e) && (gameBoard['tl'] == gameBoard['tm']) && (gameBoard['tm'] == gameBoard['tr'])){
         console.log(currentPlayer + ' is the winner!');
+        return true;
     }
-    if ((gameBoard['tl'] != e) && (gameBoard['tl'] == gameBoard['ml']) && (gameBoard['ml'] == gameBoard['bl'])){
+    else if ((gameBoard['tl'] != e) && (gameBoard['tl'] == gameBoard['ml']) && (gameBoard['ml'] == gameBoard['bl'])){
         console.log(currentPlayer + ' is the winner!');
+        return true;
     }
-    if ((gameBoard['tl'] != e) && (gameBoard['tl'] == gameBoard['mm']) && (gameBoard['mm'] == gameBoard['br'])){
+    else if ((gameBoard['tl'] != e) && (gameBoard['tl'] == gameBoard['mm']) && (gameBoard['mm'] == gameBoard['br'])){
         console.log(currentPlayer + ' is the winner!');
+        return true;
     }
-    if ((gameBoard['tm'] != e) && (gameBoard['tm'] == gameBoard['mm']) && (gameBoard['mm'] == gameBoard['bm'])){
+    else if ((gameBoard['tm'] != e) && (gameBoard['tm'] == gameBoard['mm']) && (gameBoard['mm'] == gameBoard['bm'])){
         console.log(currentPlayer + ' is the winner!');
+        return true;
     }
-    if ((gameBoard['tr'] != e) && (gameBoard['tr'] == gameBoard['mr']) && (gameBoard['mr'] == gameBoard['br'])){
+    else if ((gameBoard['tr'] != e) && (gameBoard['tr'] == gameBoard['mr']) && (gameBoard['mr'] == gameBoard['br'])){
         console.log(currentPlayer + ' is the winner!');
+        return true;
     }
-    if ((gameBoard['tr'] != e) && (gameBoard['tr'] == gameBoard['mm']) && (gameBoard['mm'] == gameBoard['bl'])){
+    else if ((gameBoard['tr'] != e) && (gameBoard['tr'] == gameBoard['mm']) && (gameBoard['mm'] == gameBoard['bl'])){
         console.log(currentPlayer + ' is the winner!');
+        return true;
     }
-    if ((gameBoard['ml'] != e) && (gameBoard['ml'] == gameBoard['mm']) && (gameBoard['mm'] == gameBoard['mr'])){
+    else if ((gameBoard['ml'] != e) && (gameBoard['ml'] == gameBoard['mm']) && (gameBoard['mm'] == gameBoard['mr'])){
         console.log(currentPlayer + ' is the winner!');
+        return true;
     }
-    if ((gameBoard['bl'] != e) && (gameBoard['bl'] == gameBoard['bm']) && (gameBoard['bm'] == gameBoard['br'])){
+    else if ((gameBoard['bl'] != e) && (gameBoard['bl'] == gameBoard['bm']) && (gameBoard['bm'] == gameBoard['br'])){
         console.log(currentPlayer + ' is the winner!');
+        return true;
+    }
+    else{
+        return false;
     }
 
+}
+
+function checkTieCondition(){
+    if (playCounter ==9){
+        console.log('TIE')
+        return true;
+    } else {
+        return false;
+    }
 }
 
